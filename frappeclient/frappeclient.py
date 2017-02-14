00163cddd79118b244e89f4abe61a0900d06900b
Labeeb Mattra <mmlabeeb@gmail.com>
2017-02-14 13:38:40 +0530

    import io updated

--- frappeclient/frappeclient.py
+++ frappeclient/frappeclient.py
@@ -1,6 +1,9 @@
 import requests
 import json
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 class AuthError(Exception):
